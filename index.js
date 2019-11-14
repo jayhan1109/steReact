b9b90fda00428b585b5843fe6df4315534755161
Jeongho Han <jungho1109@gmail.com>
2019-11-13 22:24:12 -0800

    Install cookieSession, passport initialize, session

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("./models/User");
-require("./services/passport");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const authRoutes = require("./routes/authRoutes");
 const keys = require("./config/keys");
+const User = require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
