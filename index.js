5d2ee34fa917ab4982da59f241934e8d1d25366c
Jeongho Han <jungho1109@gmail.com>
2019-11-13 19:44:50 -0800

    Add accessToken , refreshToken, profile

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken", accessToken);
+      console.log("refresh Token", refreshToken);
+      console.log("profile", profile);
     }
   )
 );
@@ -29,5 +31,3 @@ app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// http://localhost:5000/auth/google/callback?code=4%2FtAHokcvj1X1nAQkMkHmn68VR4pIBr6I1nFIiwqIWyc_ErRLuVXmGYF6khO8MoK9FyJ-2kWbkEdhr3GcQJ2kdPa0&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&session_state=491b1a58218f0fe97667e859dbfc6b5f7eb77c59..2f0f&prompt=consent#
